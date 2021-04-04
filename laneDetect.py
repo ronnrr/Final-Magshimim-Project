19ba8bd3050d96142f4f5f4715c382a154502428
itay kraise <tatytoy1@gmail.com>
2021-04-04 03:38:33 +0300

    added speed control + pep8

--- laneDetect.py
+++ laneDetect.py
@@ -2,18 +2,19 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def canny_edge_detector(image):
-    gray_image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY) 
+    gray_image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
     # Convert the image color to grayscale
 
     # Reduce noise from the image
-    blur = cv2.GaussianBlur(gray_image, (5, 5), 0) 
+    blur = cv2.GaussianBlur(gray_image, (5, 5), 0)
     canny = cv2.Canny(blur, 50, 150)
     return canny
 
+
 def region_of_interest(image):
     height = image.shape[0]
-    print(height)
     polygons = np.array([
         [(0, height), (256, height), (100, 0)]
         ])
@@ -23,8 +24,8 @@ def region_of_interest(image):
 
     # Bitwise operation between canny image and mask image
     masked_image = cv2.bitwise_and(image, mask)
-    print(masked_image)
     return masked_image
+
 
 def create_coordinates(image, line_parameters):
     slope, intercept = line_parameters
@@ -34,12 +35,12 @@ def create_coordinates(image, line_parameters):
     x2 = int((y2 - intercept) / slope)
     return np.array([x1, y1, x2, y2])
 
+
 def average_slope_intercept(image, lines):
     left_fit = []
     right_fit = []
     for line in lines:
         x1, y1, x2, y2 = line.reshape(4)
-        
         # It will fit the polynomial and the intercept and slope
         parameters = np.polyfit((x1, x2), (y1, y2), 1)
         slope = parameters[0]
@@ -48,12 +49,13 @@ def average_slope_intercept(image, lines):
             left_fit.append((slope, intercept))
         else:
             right_fit.append((slope, intercept))
-            
-    left_fit_average = np.average(left_fit, axis = 0)
-    right_fit_average = np.average(right_fit, axis = 0)
+    left_fit_average = np.average(left_fit, axis=0)
+    right_fit_average = np.average(right_fit, axis=0)
     left_line = create_coordinates(image, left_fit_average)
     right_line = create_coordinates(image, right_fit_average)
     return np.array([left_line, right_line])
+
+
 def display_lines(image, lines):
     line_image = np.zeros_like(image)
     if lines is not None:
@@ -66,13 +68,10 @@ def display_lines(image, lines):
 def detectLaneCoordiantes(frame):
     canny_image = canny_edge_detector(frame)
     cropped_image = region_of_interest(canny_image)
-    lines = cv2.HoughLinesP(cropped_image, 1, np.pi / 180, 100, 
-                            np.array([]), minLineLength = 40, 
-                            maxLineGap = 5)
-    if (lines == None):
+    lines = cv2.HoughLinesP(cropped_image, 1, np.pi / 180, 100, np.array([]), minLineLength=40, maxLineGap=5)
+    if (lines is None):
         return 0
     averaged_lines = average_slope_intercept(frame, lines)
     # line_image = display_lines(frame, averaged_lines)
-    # combo_image = cv2.addWeighted(frame, 0.8, line_image, 1, 1) 
+    # combo_image = cv2.addWeighted(frame, 0.8, line_image, 1, 1)
     return averaged_lines
-
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import os
 import time
 import laneDetect
 
+
 class remote:
     def __init__(self):
         # connect to the AirSim simulator
@@ -12,30 +13,48 @@ class remote:
         self.client.confirmConnection()
         self.client.enableApiControl(True)
         self.car_controls = airsim.CarControls()
-        self.client.reset()
+        # self.client.reset()
+        self.car_state = self.client.getCarState()
+        self.client.simSetVehiclePose(airsim.Pose(airsim.Vector3r(183, -198, 1), airsim.utils.to_quaternion(0, 0, np.pi * 3 / 2)), True)
         print("API Control enabled: %s" % self.client.isApiControlEnabled())
 
-    def drive(self):
-        a = laneDetect.detectLaneCoordiantes(self.takePicture())
-        if (a != 0): # lane detected!
-            print("prnting")
+    def __del__(self):
+        self.client.enableApiControl(False)
 
-    def checkDistance():
-        
+    def drive(self):
+        # a = laneDetect.detectLaneCoordiantes(self.takePicture())
+        # if (a != 0): # lane detected!
+        #    print("prnting")
+        self.car_state = self.client.getCarState()
+        if (self.car_state.speed > 15):
+            self.car_controls.throttle = -1
+            self.car_controls.is_manual_gear = True
+            self.car_controls.manual_gear = -1
+        elif (self.checkDistance() > 20):
+            self.car_controls.throttle = 1
+            self.car_controls.is_manual_gear = False
+            self.car_controls.manual_gear = 0
+        else:
+            self.car_controls.throttle = 0
+            self.car_controls.is_manual_gear = False
+            self.car_controls
+        self.client.setCarControls(self.car_controls)
+
+    def checkDistance(self):
+        dist = self.client.getDistanceSensorData()
+        self.client.simPrintLogMessage(f"distance {dist.distance}")
+        return dist.distance
 
     def takePicture(self):
         # get camera images from the car
-        response = self.client.simGetImages([airsim.ImageRequest("1", airsim.ImageType.Scene, False, False)])[0]  #scene vision image in uncompressed RGB array
-
-        img1d = np.fromstring(response.image_data_uint8, dtype=np.uint8) # get numpy array
-        img_rgb = img1d.reshape(response.height, response.width, 3) # reshape array to 3 channel image array H X W X 3
-        return img_rgb
+        rawImage = self.client.simGetImage("0", airsim.ImageType.Scene)
+        return cv2.imdecode(airsim.string_to_uint8_array(rawImage), cv2.IMREAD_UNCHANGED)
 
 
 def main():
     myRemote = remote()
-    myRemote.drive()
-    client.enableApiControl(False)
+    while True:
+        myRemote.drive()
 
 if __name__ == '__main__':
     main()
